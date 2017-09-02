e02fc302b63762a3fb792d707d3b04601ec46297
Junichi Kato <j5ik2o@gmail.com>
2017-09-03 00:54:08 +0900

    refactor

--- local-middleware/proxy-local-registry.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-ssh -i ~/.minikube/machines/minikube/id_rsa -N -L '*:5000:localhost:5000' docker@$(minikube ip)
-
-# kubectl port-forward --namespace kube-system $(kubectl get po -n kube-system | grep kube-registry-v0 | awk '{print $1;}') 5000:5000
